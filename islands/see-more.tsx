84eb5977854fed55009b6c1b91a03d34d8f59db3
Cesar de Barros <cbsorrilha@hotmail.com>
2023-12-26 15:45:51 -0300

    fix: conserta a island de leia mais

--- islands/see-more.tsx
+++ islands/see-more.tsx
@@ -5,12 +5,18 @@ interface SeeMoreProps {
   children: ComponentChildren;
 }
 
+const open = signal(false);
 const SeeMore = ({children}: SeeMoreProps) => {
-  const open = signal(false);
+  const value = open.value
+  const handleToggle = () => {
+    open.value = !open.value
+  }
   return (
-    <div>
-      <button onClick={() => open.value = !open}>Ver mais</button>
-      <div class={`${open ? "h-full" : "h-0"}`}>
+    <div class="flex justify-center flex-col items-center mt-5">
+      <button class="font-bold text-center text-white border-solid border-2 border-gray-50 bg-gray-400 px-5 w-fit" onClick={handleToggle}>
+        Ver {!value ? 'mais' : 'menos'}
+      </button>
+      <div class={`${value ? "h-96" : "h-0"} overflow-hidden transition-all delay-200 ease-linear`}>
         {children}
       </div>
     </div>
