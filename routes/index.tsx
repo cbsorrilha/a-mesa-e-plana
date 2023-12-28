4143356d2af5807a44c937cea1cd3eed45e991fe
Cesar de Barros <cbsorrilha@hotmail.com>
2023-12-28 10:51:52 -0300

    fix: atualiza os estilos pra deixar menos zuado no mobile

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,17 +12,29 @@ export default async function Home() {
     <div class="w-screen h-screen overflow-hidden bg-no-repeat bg-cover" style={{ backgroundImage: `url(${heroIMG})` }}>
       <div class="w-screen h-screen bg-black opacity-75">
         <div class="flex flex-col justify-center h-full">
-            <div class="w-6/12 bg-black mx-auto h-100">
-              <div class="text-3xl font-bold text-center text-white">PROXIMA SESSÃO</div>
+            <div class="bg-black mx-auto h-100 w-10/12 lg:w-8/12">
+              <div class="text-1xl font-bold text-center text-white w-full">PROXIMA SESSÃO</div>
               <div class="text-4xl font-bold text-center text-white">{nextSession.title}</div>
-              <div class="text-2xl font-bold text-center text-white">Onde? {nextSession.place}</div>
-              <div class="text-2xl font-bold text-center text-white">Quando? {formatDate(nextSession.date)}</div>
-              <div class="text-2xl font-bold text-center text-white">GM:{nextSession.gm}</div>
+              <div class="font-bold text-center text-white">
+                <div class="text-l">
+                  Onde? 
+                </div>
+                <div class="text-2xl ml-3">{nextSession.place}</div>
+              </div>
+              <div class="font-bold text-center text-white">
+                <div class="text-l">Quando? </div>
+                <div class="text-2xl">{formatDate(nextSession.date)}</div>
+              </div>
+              <div class="font-bold text-center text-white">
+                <div class="text-l">GM: </div>
+                <div class="text-2xl">{nextSession.gm}</div>
+                
+              </div>
               <SeeMore>
                 <div class="text-l font-bold text-center text-white">Descrição:</div>
-                <div class="text-2xl font-bold text-center text-white">{nextSession.description}</div>
+                <div class="text-xl font-bold text-center text-white text-justify">{nextSession.description}</div>
                 <div class="text-l font-bold text-center text-white">Resumo:</div>
-                <div class="text-xl font-bold text-center text-white">{nextSession.summary}</div>
+                <div class="text-xl font-bold text-center text-white overflow-y-scroll h-40 text-justify">{nextSession.summary}</div>
               </SeeMore>
 
             </div>
